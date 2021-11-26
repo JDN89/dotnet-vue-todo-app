de7082433004afa0c7679ecbd120b5d814b1315d
Jan De Niels <jan.de.niels@gmail.com>
2021-11-25 20:25:09 -0500

    change styling

--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts"></script>
 
 <template>
+  <p>
+    register if you're a new user or login to create and use your to-do lists.
+    or leave a message on the messageboard if you just came by.
+  </p>
   <MessageBoard />
 </template>
 
