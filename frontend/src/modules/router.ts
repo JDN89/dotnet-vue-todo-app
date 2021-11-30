09a79097b0f47f1c8fa267688e91bcffc87a7ab2
Jan De Niels <jan.de.niels@gmail.com>
2021-11-30 17:46:37 -0500

    finish custom router component

--- frontend/src/modules/router.ts
+++ frontend/src/modules/router.ts
@@ -1,15 +1,29 @@
 import { UserModule } from "~/types";
 import { useUserStore } from "~/stores/user";
 
+//custom router module
+
 export const install: UserModule = ({ app, router, isClient }) => {
   if (isClient) {
     // use const inside export function, otherwise error that pinis is not isntalled yet
     const userStore = useUserStore();
-    router.beforeEach((to, from) => {
-      console.log(userStore.getIsAuthenticated);
+    router.beforeEach((to, from, next) => {
+      if (to.meta?.requiresAuth && userStore.getIsAuthenticated) {
+        console.log("authorized and required");
 
-      console.log(to);
-      console.log(from);
+        next();
+      } else if (to.meta?.requiresAuth) {
+        console.log("required and unauthorized");
+
+        next("/login");
+      } else {
+        next();
+      }
+
+      //   console.log(userStore.getIsAuthenticated);
+
+      //   console.log(to);
+      //   console.log(from);
     });
   }
 };
