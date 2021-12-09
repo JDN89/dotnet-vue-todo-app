f27244f3c69c2761fb525fe7b5d7ced57092df7c
Jan De Niels <jan.de.niels@gmail.com>
2021-12-09 13:34:36 -0500

    setup swagger

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,5 +1,25 @@
+using Dapper;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
+
+
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/error");
+}
+
+app.MapGet("/error", () => Results.Problem("An error occurred.", statusCode: 500))
+   .ExcludeFromDescription();
+
+app.MapSwagger();
+app.UseSwaggerUI();
+
+
 
 app.MapGet("/", () => "Hello World!");
 
