97115f98c4c8b4b608fe05b207853f1a94ce261c
Jan De Niels <jan.de.niels@gmail.com>
2021-12-09 18:53:26 -0500

    connect postgress and dapper to api

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,11 +1,19 @@
 using Dapper;
+using Npgsql;
+using System.ComponentModel.DataAnnotations;
+
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = "User ID=jan; Password=9450; Host=localhost; Port=5432; Database=postgres; Pooling=true;";
+
+builder.Services.AddScoped(_ => new NpgsqlConnection(connectionString));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
+await EnsureDb(app.Services, app.Logger);
 
 
 if (!app.Environment.IsDevelopment())
@@ -21,6 +29,28 @@ app.UseSwaggerUI();
 
 
 
+
 app.MapGet("/", () => "Hello World!");
 
+app.MapGet("/hello", async (NpgsqlConnection db) =>
+    await db.QueryAsync<Message>("SELECT * FROM public.message_board"))
+   .WithName("GetAllMessages");
+
 app.Run();
+
+
+async Task EnsureDb(IServiceProvider services, ILogger logger)
+{
+    logger.LogInformation("Ensuring database exists at connection string '{connectionString}'", connectionString);
+
+    using var db = services.CreateScope().ServiceProvider.GetRequiredService<NpgsqlConnection>();
+
+}
+
+public class Message
+{
+    public int Id { get; set; }
+    [Required]
+    public string? Title { get; set; }
+    public string? Body { get; set; }
+}
