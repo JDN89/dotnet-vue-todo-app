a2352f310d70b73f54b87bc2e507ab38d0ea349e
Jan De Niels <jan.de.niels@gmail.com>
2021-12-11 12:41:08 -0500

    practice routing parameters

--- TodoApi/modules/MessageModule/Messageboard.cs
+++ TodoApi/modules/MessageModule/Messageboard.cs
@@ -1,24 +1,36 @@
 namespace MessageBoard;
 
+
 using Dapper;
 using Npgsql;
 using Carter;
-
+using Microsoft.AspNetCore.Mvc;
 using TodoApi.modules.MessageModule.models;
 
 public class MessageModule : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapGet("/", GetMessages);
+        // app.MapGet("/", GetMessages);
+        app.MapGet("/{id}", GetMessage);
+
 
 
     }
-    private async Task<IEnumerable<Message>> GetMessages(NpgsqlConnection db) =>
-        await db.QueryAsync<Message>("SELECT * FROM public.message_board");
+    // private async Task<IEnumerable<Message>> GetMessages(NpgsqlConnection db) =>
+    //     await db.QueryAsync<Message>("SELECT * FROM public.message_board");
+    private async Task<Message> GetMessage(int id, NpgsqlConnection db) =>
+        await db.QuerySingleOrDefaultAsync<Message>("SELECT * FROM public.messages WHERE id = @id", new { id });
 
 }
 
+
+//    private static async Task<IResult> GetTodo(int id, SqliteConnection db) =>
+//         await db.QuerySingleOrDefaultAsync<Todo>(
+//             "SELECT * FROM Todos WHERE Id = @id", new { id })
+//             is Todo todo
+//                 ? Results.Ok(todo)
+//                 : Results.NotFound();
 
 
 //         app.MapGet("/", () => "Hello World!");
