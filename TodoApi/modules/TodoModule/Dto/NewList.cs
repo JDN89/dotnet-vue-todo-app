d6bc9fae9e9f7b4e09c768fd717c0e5d4a2cab26
Jan De Niels <jan.de.niels@gmail.com>
2021-12-13 16:34:21 -0500

    add userId in route parameter and retrieve lists related to said id

--- TodoApi/modules/TodoModule/Dto/NewList.cs
+++ TodoApi/modules/TodoModule/Dto/NewList.cs
@@ -2,7 +2,7 @@ namespace TodoApi.modules.TodoModule.Dto
 {
     public class NewList
     {
-        public int UserID { get; set; }
+        public int ListID { get; set; }
         public string? Title { get; set; }
         public String[]? Todos { get; set; }
         public String[]? Archived { get; set; }
--- TodoApi/modules/TodoModule/Todos.cs
+++ TodoApi/modules/TodoModule/Todos.cs
@@ -12,15 +12,16 @@ public class TodosModule : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapGet("/test", FetchList);
+        app.MapGet("/{userId}", FetchList);
 
 
 
     }
 
 
-    private async Task<IEnumerable<NewList>> FetchList(NpgsqlConnection db) =>
-          await db.QueryAsync<NewList>("SELECT L.id as userId , L.title ,ARRAY_AGG(T.todo) as Todos ,ARRAY_AGG(A.archived) as Archived FROM todo_lists L inner join todos T on(L.id = T.list_id)inner join archived_todos A on(L.id = A.list_id)group by L.id, L.title ");
+    private async Task<IEnumerable<NewList>> FetchList(int id, NpgsqlConnection db) =>
+          await db.QueryAsync<NewList>("SELECT L.id as ListId , L.title ,ARRAY_AGG(T.todo) as Todos ,ARRAY_AGG(A.archived) as Archived FROM todo_lists L inner join todos T on(L.id = T.list_id)inner join archived_todos A on(L.id = A.list_id) where L.user_id =@id group by L.id, L.title ", new { id });
 
 }
+
 
