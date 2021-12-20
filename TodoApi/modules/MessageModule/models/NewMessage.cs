93ae56ef850a6e01345327dc9d93c58d2b3887b9
Jan De Niels <jan.de.niels@gmail.com>
2021-12-20 15:07:32 -0500

    add NewMessage model

---
+++ TodoApi/modules/MessageModule/models/NewMessage.cs
@@ -0,0 +1,8 @@
+namespace TodoApi.modules.MessageModule.models
+{
+    public class NewMessage
+    {
+        public string? Title { get; set; }
+        public string? Body { get; set; }
+    }
+}
