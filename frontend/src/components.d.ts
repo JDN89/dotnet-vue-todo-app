4458a58b4c5a5594e6761951f420988e4bc048c2
Jan De Niels <jan.de.niels@gmail.com>
2021-12-22 10:00:16 -0500

    update outdated npm packages

--- frontend/src/components.d.ts
+++ frontend/src/components.d.ts
@@ -4,25 +4,17 @@
 
 declare module 'vue' {
   export interface GlobalComponents {
-    BaseNavBarGuest: typeof import("./components/BaseNavBarGuest.vue")["default"];
     CarbonAddAlt: typeof import('~icons/carbon/add-alt')['default']
     CarbonCampsite: typeof import('~icons/carbon/campsite')['default']
-    CarbonCheckbox: typeof import('~icons/carbon/checkbox')['default']
-    CarbonCheckboxChecked: typeof import('~icons/carbon/checkbox-checked')['default']
-    CarbonDicomOverlay: typeof import("~icons/carbon/dicom-overlay")["default"];
     CarbonEdit: typeof import('~icons/carbon/edit')['default']
     CarbonGroupAccount: typeof import('~icons/carbon/group-account')['default']
-    CarbonGroupLogout: typeof import("~icons/carbon/group-logout")["default"];
     CarbonLanguage: typeof import('~icons/carbon/language')['default']
     CarbonLogin: typeof import('~icons/carbon/login')['default']
     CarbonLogoGithub: typeof import('~icons/carbon/logo-github')['default']
-    CarbonLogout: typeof import('~icons/carbon/logout')['default']
     CarbonMoon: typeof import('~icons/carbon/moon')['default']
-    CarbonPedestrian: typeof import("~icons/carbon/pedestrian")["default"];
     CarbonSun: typeof import('~icons/carbon/sun')['default']
     CarbonTrashCan: typeof import('~icons/carbon/trash-can')['default']
     CarbonUpload: typeof import('~icons/carbon/upload')['default']
-    CarbonWarning: typeof import("~icons/carbon/warning")["default"];
     CreateNewMessage: typeof import('./components/Messages/message/CreateNewMessage.vue')['default']
     Message: typeof import('./components/Messages/message/Message.vue')['default']
     MessageBoard: typeof import('./components/Messages/MessageBoard.vue')['default']
@@ -30,9 +22,7 @@ declare module 'vue' {
     NavBarUser: typeof import('./components/NavBarUser.vue')['default']
     NewList: typeof import('./components/todos/NewList.vue')['default']
     README: typeof import('./components/README.md')['default']
-    TheHeader: typeof import("./components/TheHeader.vue")["default"];
     TodoList: typeof import('./components/todos/TodoList.vue')['default']
-    Todos: typeof import("./components/todos/Todos.vue")["default"];
     UpdateMessage: typeof import('./components/Messages/message/UpdateMessage.vue')['default']
   }
 }
