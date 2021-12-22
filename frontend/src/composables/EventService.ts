9f003027ce348ad545e59e1c395488578d061e0a
Jan De Niels <jan.de.niels@gmail.com>
2021-12-22 15:44:14 -0500

    axios setup failed +> chore:fix

---
+++ frontend/src/composables/EventService.ts
@@ -0,0 +1,25 @@
+// import axios from "axios";
+
+
+
+// export default  {
+//    apiClient = axios.create({
+//     baseURL: "https://localhost:7126",
+//     withCredentials: false,
+//     headers: {
+//       Accept: "application/json",
+//       "Content-Type": "application/json",
+//     },
+//   });
+  
+// };
+
+
+
+// getEvents() {
+//  return apiClient.get("/");
+// },
+
+// postEvents(message) {
+//   return apiClient.post("/" + message);
+// },
