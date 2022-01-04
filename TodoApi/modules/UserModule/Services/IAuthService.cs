2e3e38f24ef737b9f1484ebdbae8c642c4369e38
Jan De Niels <jan.de.niels@gmail.com>
2022-01-04 13:39:11 -0500

    remove unused using statements

--- TodoApi/modules/UserModule/Services/IAuthService.cs
+++ TodoApi/modules/UserModule/Services/IAuthService.cs
@@ -1,9 +1,3 @@
-using TodoApi.modules.UserModule.Models;
-using TodoApi.modules.UserModule.Dto;
-using Npgsql;
-
-
-
 namespace TodoApi.modules.UserModule.Services
 {
     public interface IAuthService
