67b21d91212ef41a604ef68390e90b2d3c93387e
Jan De Niels <jan.de.niels@gmail.com>
2022-01-10 20:00:16 -0500

    add Token Dto so you can pass Token + Id to the frontend

---
+++ TodoApi/modules/UserModule/Dto/TokenDto.cs
@@ -0,0 +1,14 @@
+namespace TodoApi.modules.UserModule.Dto
+{
+    public class TokenDto
+    {
+        public int? UserdId { get; set; }
+
+        public string? Token { get; set; }
+        
+        
+
+        
+        
+    }
+}
--- TodoApi/modules/UserModule/Dto/UserDto.cs
+++ TodoApi/modules/UserModule/Dto/UserDto.cs
@@ -5,5 +5,6 @@ namespace TodoApi.modules.UserModule.Dto
         public string? Email { get; set; }
 
         public string? PassWord { get; set; }
+
     }
 }
