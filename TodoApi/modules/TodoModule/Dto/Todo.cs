8fe12cfaefc4b8bc037c76d7000d4393f44a31af
Jan De Niels <jan.de.niels@gmail.com>
2022-01-16 21:37:12 -0500

    refactor code based on webstorm recomendations

--- TodoApi/modules/TodoModule/Dto/Todo.cs
+++
@@ -1,9 +0,0 @@
-namespace TodoApi.modules.TodoModule.Dto
-{
-    public class Todo
-    {
-        public int Id { get; set; }
-        public int? ListId { get; set; }
-        public String[]? Todos { get; set; }
-    }
-}
