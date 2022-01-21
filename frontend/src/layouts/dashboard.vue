e4e30914d20bfd3a317c84a04a07269445a1e6fc
Jan De Niels <jan.de.niels@gmail.com>
2022-01-21 18:38:21 -0500

    remove [todos] from dashboard layout

--- frontend/src/layouts/dashboard.vue
+++ frontend/src/layouts/dashboard.vue
@@ -2,6 +2,6 @@
   <main class="px-4 py-10 text-center text-gray-700 dark:text-gray-200">
     <NavBarUser />
     <router-view />
-    <div class="mt-5 mx-auto text-center opacity-25 text-sm">[Todos]</div>
+    <div class="mt-5 mx-auto text-center opacity-25 text-sm"></div>
   </main>
 </template>
