6122c3fc6406dc517748624319a5cbb408ae7194
Jan De Niels <jan.de.niels@gmail.com>
2022-02-01 13:36:40 -0500

    create messageboard db if it doesn't exists

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -164,7 +164,7 @@ async Task EnsureDb(IServiceProvider services, ILogger logger)
     await using var db = services.CreateScope().ServiceProvider.GetRequiredService<NpgsqlConnection>();
     
     var sql = $@"CREATE TABLE IF NOT EXISTS messages (
-	id int4 NOT NULL DEFAULT nextval('message_board_message_id_seq'::regclass),
+	id int4 NOT NULL,
 	title text NOT NULL,
 	body text NOT NULL,
 	CONSTRAINT message_board_pk PRIMARY KEY (id)
@@ -179,4 +179,5 @@ async Task EnsureDb(IServiceProvider services, ILogger logger)
     
     
     await db.ExecuteAsync(sql);
+    await db.ExecuteAsync(sql1);
 }
