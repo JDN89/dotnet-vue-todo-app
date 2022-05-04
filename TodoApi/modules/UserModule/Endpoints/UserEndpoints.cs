63b4c6cc7a90388bc4f0f1a78f7bdc36fbd4df79
JDN89 <jan.de.niels@gmail.com>
2022-05-04 14:12:34 -0500

    refactor register and login endpoints

--- TodoApi/modules/UserModule/Endpoints/UserEndpoints.cs
+++ TodoApi/modules/UserModule/Endpoints/UserEndpoints.cs
@@ -2,13 +2,14 @@ using Dapper;
 using FluentValidation;
 using FluentValidation.Results;
 using Npgsql;
+using TodoApi.modules.MessageModule.Endpoints.Internal;
 using TodoApi.modules.UserModule.Dto;
 using TodoApi.modules.UserModule.Models;
 using TodoApi.modules.UserModule.Services;
 
 namespace TodoApi.modules.UserModule.Endpoints;
 
-public class UserEndpoints
+public class UserEndpoints : IEndpoints
 {
     private const string ContentType = "application/json";
     private const string Tag = "Users";
@@ -19,6 +20,13 @@ public class UserEndpoints
             .WithName("RegisterUser")
             .Accepts<UserDto>(ContentType)
             .Produces<int>(201).Produces<IEnumerable<ValidationFailure>>(400)
+            .WithTags(Tag)
+            .AllowAnonymous();
+
+        app.MapPost("api/login", Login)
+            .WithName("LoginUser")
+            .Accepts<UserDto>(ContentType)
+            .Produces<int>(200).Produces<IEnumerable<ValidationFailure>>(400).Produces(404).Produces(401)
             .WithTags(Tag)
             .AllowAnonymous();
     }
@@ -40,7 +48,7 @@ public class UserEndpoints
         }
 
 // null check not necessary => UserDto validator checks for null
-        var hash = await EncryptionService.EncryptPassword(oUser.PassWord!);
+        var hash = await encryptionService.EncryptPasswordAsync(oUser.PassWord!);
 
         var newUser = new User
         {
@@ -54,13 +62,37 @@ public class UserEndpoints
         return Results.Created("api/register", newUserId);
     }
 
+    private static async Task<IResult> Login(UserDto oUser, NpgsqlConnection db, IEncryptionService encryptionService,
+        ITokenService tokenService, IValidator<UserDto> validator)
+    {
+        var validationResult = await validator.ValidateAsync(oUser);
+        if (!validationResult.IsValid)
+        {
+            return Results.BadRequest(validationResult.Errors);
+        }
+
+        //check if oUser exists in db
+        var user = await db.QueryFirstOrDefaultAsync<User>("SELECT * FROM users Where email=@Email", oUser);
+        if (user is null)
+            return Results.NotFound("user not found");
+
+        //verify user his password
+        var verified = await encryptionService.VerifyPasswordAsync(oUser.PassWord!, user.Hash!);
+        if (!verified)
+            return Results.Unauthorized();
+
+
+        var token = tokenService.CreateToken(user);
+
+        return Results.Ok(token);
+    }
+
 
 // ======================================    
 // insert Services to which you offload extra logic here
 // ===========================================
     public static void AddServices(IServiceCollection services, IConfiguration configuration)
     {
-        services.AddSingleton<IUserService, UserService>();
         services.AddSingleton<ITokenService, TokenService>();
         services.AddSingleton<IEncryptionService, EncryptionService>();
     }
--- TodoApi/modules/UserModule/Services/TokenService.cs
+++ TodoApi/modules/UserModule/Services/TokenService.cs
@@ -43,6 +43,6 @@ public class TokenService : ITokenService
         var token = tokenHandler.CreateToken(tokenDescriptor);
 
         return tokenHandler.WriteToken(token);
-        ;
+        
     }
 }
