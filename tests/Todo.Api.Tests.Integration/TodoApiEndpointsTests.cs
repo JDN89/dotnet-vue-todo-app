d3bd29b3486d28bea9d6cc0ae2ad28e451a90632
JDN89 <jan.de.niels@gmail.com>
2022-05-05 14:39:18 -0500

    add integration tests

---
+++ tests/Todo.Api.Tests.Integration/TodoApiEndpointsTests.cs
@@ -0,0 +1,74 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using TodoApi;
+using TodoApi.modules.MessageModule.models;
+using Xunit;
+
+namespace Todo.Api.Tests.Integration;
+// access httpclient
+// WebApplicationFactory access a full fledged api
+// this api can't be called  through the web, only via httpclient
+// in memory api perfect for testing
+public class TodoApiEndpointsTests:IClassFixture<WebApplicationFactory<IApiMarker>>
+{
+    private readonly WebApplicationFactory<IApiMarker> _factory;
+    
+
+    public TodoApiEndpointsTests(WebApplicationFactory<IApiMarker> factory)
+    {
+        _factory = factory;
+        
+    }
+
+    [Fact]
+    public async Task Create_CreateMessage_WhenDataIsCorrect()
+    {
+//Arrange
+    var httpClient = _factory.CreateClient();
+
+    var message = GenerateMessage();
+//Act
+        var result = await httpClient.PostAsJsonAsync("api/", message);
+        var createdMessage =await result.Content.ReadFromJsonAsync<Message>();
+//Assert
+        result.StatusCode.Should().Be(HttpStatusCode.Created);
+        createdMessage.Should().BeEquivalentTo(message);
+        result.Headers.Location.Should().Be($"api/");
+    }
+    
+
+    [Fact]
+    public async Task CreateMessage_Fails_WhenTitleISInvalid()
+    {
+//Arrange
+    var httpClient = _factory.CreateClient();
+
+    var message = GenerateMessage();
+    message.Title = "";
+//Act
+        var result = await httpClient.PostAsJsonAsync("api/", message);
+        var errors = await result.Content.ReadFromJsonAsync<IEnumerable<ValidationError>>();
+        var error = errors!.Single();
+        
+//Assert
+        result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        error.PropertyName.Should().Be("Title");
+        error.ErrorMessage.Should().Be("'Title' must not be empty.");
+    }
+    
+    private NewMessage GenerateMessage(string title = "test title", string body = "test body")
+    {
+        return new NewMessage
+        {
+            Title = title,
+            Body = body
+
+
+        };
+    }
+}
